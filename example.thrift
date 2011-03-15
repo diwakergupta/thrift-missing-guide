bc9f23e9de9eb0ae3ebabd45961a07d5834a0fdd
Diwaker Gupta <diwakergupta@gmail.com>
2011-03-15 15:12:11 -0700

    Several improvements.
    - split guide into separate files for easier maintenance
    - include an example thrift file
    - start Java/C++ code generation sections
    - add revision date and remark about Thrift version

---
+++ example.thrift
@@ -0,0 +1,36 @@
+namespace cpp thrift.example
+namespace java thrift.example
+
+enum TweetType {
+    TWEET,
+    RETWEET = 2,
+    DM = 0xa,
+    REPLY
+}
+
+struct Location {
+    1: required double latitude;
+    2: required double longitude;
+}
+
+struct Tweet {
+    1: required i32 userId;
+    2: required string userName;
+    3: required string text;
+    4: optional Location loc;
+    5: optional TweetType tweetType = TweetType.TWEET;
+    16: optional string language = "english";
+}
+
+struct TweetSearchResult {
+    1: list<Tweet> tweets;
+}
+
+const i32 MAX_RESULTS = 100;
+
+service Twitter {
+    void ping(),
+    bool postTweet(1:Tweet tweet);
+    TweetSearchResult searchTweets(1:string query);
+    oneway void zip()
+}
