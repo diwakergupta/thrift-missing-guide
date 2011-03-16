8bab7bc678976a70eda132d8a86ffa3a6e010e32
Diwaker Gupta <diwakergupta@gmail.com>
2011-03-15 18:27:29 -0700

    Enable PDF generation.
    Also add a note about naming conventions in Java.

--- Makefile
+++ Makefile
@@ -8,8 +8,20 @@ default:
 		-o index.html \
 		thrift.asciidoc
 
+pdf:
+	a2x --fop \
+		-a toc \
+		-a data-uri \
+		-a docinfo \
+		-a icons \
+		-a iconsdir=/usr/local/etc/asciidoc/images/icons \
+		-a pygments \
+		thrift.asciidoc
+
+all: default pdf
+
 clean:
 	rm -f index.html *.png
 
-publish: default
+publish: default pdf
 	./publish.sh
--- publish.sh
+++ publish.sh
@@ -5,12 +5,12 @@ set -o nounset                              # Treat unset variables as an error
 rm -rf pages
 git clone -b gh-pages \
     git@github.com:diwakergupta/thrift-missing-guide.git pages
-mv index.html pages
+mv index.html thrift.pdf pages
 
 pushd pages
 git config user.name "Diwaker Gupta"
 git config user.email diwakergupta@gmail.com
-git add -u
+git add index.html thrift.pdf
 git commit -m Update
 git push origin HEAD:gh-pages
 popd
